b1485d47217230cd0ed43581be329903afec44fd
ghostMan <maxjamin@gmail.com>
2020-08-01 15:57:39 -0700

    Working on main loop

--- main.cpp
+++ main.cpp
@@ -10,61 +10,68 @@
 #include <poll.h>
 #include <iostream>
 
+#define BACKLOG 10   // how many pending connections queue will hold
+
 class Server
 {
 private:
-	int sockfd, new_fd;
-	struct addrinfo hints, *servinfo, *p;
-	struct sockaddr_storage their_addr;
-	socklen_t sin_size;
-	int yes=1;
-	char s[INET6_ADDRSTRLEN];
-	int rv;
-	char port[100] = "3499";
+    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_storage their_addr; // connector's address information
+    socklen_t sin_size;
+    //struct sigaction sa;
+    int yes=1;
+    char s[INET6_ADDRSTRLEN];
+    int rv;
+	char port[100] = "3485";
 
 public:
 	int InitializeServer(char *arg[])
 	{
 		memset(&hints, 0, sizeof hints);
-		hints.ai_family = AF_UNSPEC;
-		hints.ai_socktype = SOCK_STREAM;
-		hints.ai_flags = AI_PASSIVE; //using my ip
+	    hints.ai_family = AF_UNSPEC;
+	    hints.ai_socktype = SOCK_STREAM;
+	    hints.ai_flags = AI_PASSIVE; // use my IP
 
+	    if ((rv = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) {
+	        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+	        return 1;
+	    }
 
-		if((rv = getaddrinfo(arg[1], port, &hints, &servinfo)) != 0)
-		{
-			fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-			return 1;
-		}
-		
-		for(p = servinfo; p != NULL; p=p->ai_next) 
-		{
-			sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
-			if(listen < 0)
-			{
-				continue;
-			}
+	    // loop through all the results and bind to the first we can
+	    for(p = servinfo; p != NULL; p = p->ai_next) {
+	        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+	                p->ai_protocol)) == -1) {
+	            perror("server: socket");
+	            continue;
+	        }
 
-			if(bind(sockfd, p->ai_addr, p->ai_addrlen) < 0) {
-				close(sockfd);
-				continue;
-			}
+	        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+	                sizeof(int)) == -1) {
+	            perror("setsockopt");
+	            exit(1);
+	        }
 
-			break;
-		}
+	        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+	            close(sockfd);
+	            perror("server: bind");
+	            continue;
+	        }
 
-		//check if not Bound
-		if(p == NULL)
-		{
-			return 1;
-		}
-		freeaddrinfo(servinfo); //Done with this
+	        break;
+	    }
 
-		//now Listen
-		if (listen(sockfd, 10) == -1) {
-			return 1;
-		}
-		return 0;
+	    freeaddrinfo(servinfo); // all done with this structure
+
+	    if (p == NULL)  {
+	        fprintf(stderr, "server: failed to bind\n");
+	        exit(1);
+	    }
+
+	    if (listen(sockfd, BACKLOG) == -1) {
+	        perror("listen");
+	        exit(1);
+	    }
 	}
 
 	Server(char *argv[])
@@ -89,40 +96,45 @@ public:
 
 	int mainLoop()
 	{
+		printf("server: waiting for connections...\n");
+		char buf[256];    // Buffer for client data
 
-		// Start off with room for 5 connections
-    	// (We'll realloc as necessary)
-    	int fd_count = 0;
-    	int fd_size = 5;
-    	struct pollfd *pfds = (struct pollfd *)malloc(sizeof *pfds *fd_size);
-
-		// Add the listener to set
-    	pfds[0].fd = sockfd;
-    	pfds[0].events = POLLIN; // Report ready to read on incoming connection
-
-
-		while(1) 
-		{
-	        int poll_count = poll(pfds, fd_count, -1);
-
-	        if (poll_count == -1) {
-	            perror("poll");
-	            exit(1);
+	    while(1) {  // main accept() loop
+	        sin_size = sizeof their_addr;
+	        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+	        if (new_fd == -1) {
+	            perror("accept");
+	            continue;
 	        }
 
-	        //loop through connections
-	        for(int i = 0; i < fd_count; i++)
-	        {
-				//check listener for new connections
+	        inet_ntop(their_addr.ss_family,
+	            get_in_addr((struct sockaddr *)&their_addr),
+	            s, sizeof s);
+	        printf("server: got connection from %s\n", s);
 
-				//pass connection to new 
+	        if (!fork()) { // this is the child process
 
+				close(sockfd); // child doesn't need the listener
+	            if (send(new_fd, "Hello, world!", 13, 0) == -1)
+	                perror("send");
 
+	            while(1)
+	            {
+	            	int nbytes = recv(new_fd, buf, sizeof buf, 0);
+	            	if(nbytes == 0)
+	            	{
+	            		break;
+	            	}
+	            	if (send(new_fd, "Hello, world!", 13, 0) == -1)
+	                	perror("send");
+	            }
 
+	            close(new_fd);
+	            exit(0);
 	        }
-		}
+	        close(new_fd);  // parent doesn't need this
+	    }
 	}
-
 
 };
 
